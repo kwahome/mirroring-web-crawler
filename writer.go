123b12d0f9a772442c98047ede40784563f38c50
Wahome Macharia <wahome@Wahomes-MacBook-Pro.local>
2023-04-20 21:59:39 +0300

    Add a http client, links parsers, and file writer

---
+++ writer.go
@@ -0,0 +1,44 @@
+package crawler
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func writeToFile(data []byte, filePath string) error {
+	fmt.Printf("Writing to: '%s'\n", filePath)
+
+	err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+	if err != nil {
+		fmt.Printf("An error has occurred: '%v'\n", err)
+
+		return err
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("An error has occurred: '%v'\n", err)
+
+		return err
+	}
+
+	defer closeFile(file)
+
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Printf("An error has occurred: '%v'\n", err)
+
+		return err
+	}
+
+	return nil
+}
+
+func closeFile(f *os.File) {
+	err := f.Close()
+	if err != nil {
+		log.Fatalf("an error has occurred while closing file: %v\n", err)
+	}
+}
